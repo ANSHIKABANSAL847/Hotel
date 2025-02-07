8fb758292518863b3e3a5ea331b1a205ba22e469
ANSHIKABANSAL847 <bansalanshika847@gmail.com>
2025-02-07 21:21:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-
 import React from "react";
-
-import NavigationBar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavigationBar from "./Components/Navbar";  // Ensure correct import
+import Home from "./Components/Home";
+import Rooms from "./Components/Rooms";
+import ContactUs from "./Components/Contact";
+import About from "./Components/About";
+import Gallery from "./Components/Gallery";
+import Location from "./Components/Location";
+import Services from "./Components/Services";
+import Login from "./Components/Login";
 
 const App = () => {
   return (
-    <div>
-      <NavigationBar/>
-    </div>
-   
+    <BrowserRouter>
+      <NavigationBar /> {/* ✅ Add Navbar inside BrowserRouter */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
